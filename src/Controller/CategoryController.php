9456c56c2c721f58d73fe9517ab54afc33f46c7b
Natacha <villeneuve.natacha@gmail.com>
2020-05-30 09:27:38 +0200

    création de la classe categorycontrolller

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,41 @@
+<?php
+// src/Controller/CategoryController.php
+namespace App\Controller;
+
+use App\Entity\Category;
+use App\Form\CategoryType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CategoryController extends AbstractController
+{
+    /**
+     * @Route ("/add", name="category_add")
+     * @param Request $request
+     * @return Response
+     */
+    public function add(Request $request): Response
+    {
+        $category = new Category();
+        $form = $this->createForm(
+            CategoryType::class,
+            $category
+        );
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($data);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('wild_index');
+        }
+
+        return $this->render('category/add.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -170,32 +170,4 @@ class WildController extends AbstractController
            'season' => $season
        ]);
     }
-
-    /**
-     * @Route ("/add", name="category_add")
-     * @param Request $request
-     * @return Response
-     */
-    public function add(Request $request): Response
-    {
-        $category = new Category();
-        $form = $this->createForm(
-            CategoryType::class,
-            $category
-        );
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-            $data = $form->getData();
-
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($data);
-            $entityManager->flush();
-
-            return $this->redirectToRoute('wild_index');
-        }
-
-            return $this->render('category/add.html.twig', [
-                'form' => $form->createView(),
-            ]);
-    }
 }
