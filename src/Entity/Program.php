243d882e134a8e0cb8d70bbb1d471210d09f1a87
Natacha <villeneuve.natacha@gmail.com>
2020-06-01 16:53:37 +0200

    ajout de la table Actor

--- src/Entity/Program.php
+++ src/Entity/Program.php
@@ -36,9 +36,15 @@ class Program
      * @ORM\OneToMany(targetEntity=Season::class, mappedBy="program")
      */
     private $seasons;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="App\Entity\Actor", mappedBy="programs")
+     */
+    private $actors;
     public function __construct()
     {
         $this->seasons = new ArrayCollection();
+        $this->actors = new ArrayCollection();
     }
     public function getId(): ?int
     {
@@ -104,6 +110,34 @@ class Program
                 $season->setProgram(null);
             }
         }
+        return $this;
+    }
+
+    /**
+     * @return Collection|Actor[]
+     */
+    public function getActors(): Collection
+    {
+        return $this->actors;
+    }
+
+    public function addActor(Actor $actor): self
+    {
+        if (!$this->actors->contains($actor)) {
+            $this->actors[] = $actor;
+            $actor->addProgram($this);
+        }
+
+        return $this;
+    }
+
+    public function removeActor(Actor $actor): self
+    {
+        if ($this->actors->contains($actor)) {
+            $this->actors->removeElement($actor);
+            $actor->removeProgram($this);
+        }
+
         return $this;
     }
 }
