612de1a9474f206b870cecf8c4ce41f6e9d88365
Natacha <villeneuve.natacha@gmail.com>
2020-06-07 09:45:51 +0200

    ajout de seasonFixtures

---
+++ src/DataFixtures/SeasonFixtures.php
@@ -0,0 +1,40 @@
+<?php
+
+
+namespace App\DataFixtures;
+
+
+use App\Entity\Season;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+use Faker;
+
+class SeasonFixtures extends Fixture implements DependentFixtureInterface
+{
+
+    public function getDependencies()
+    {
+        return [ProgramFixtures::class];
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $faker  =  Faker\Factory::create('fr_FR');
+
+        for ($i=0; $i<50; $i++) {
+            $season = new Season();
+            $season->setNumber($faker->numberBetween(1,10));
+            $season->setYear($faker->year);
+            $season->setDescription($faker->text);
+            $season->setProgram($this->getReference('program_' . rand(0,5)));
+            $manager->persist($season);
+            $this->addReference('season_'. $i, $season);
+
+        }
+        $manager->flush();
+
+
+    }
+
+}
