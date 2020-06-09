c36044e1b041d24b555c67f513a642844689d57e
Natacha <villeneuve.natacha@gmail.com>
2020-06-09 15:44:39 +0200

    modifications

--- src/Service/Slugify.php
+++ src/Service/Slugify.php
@@ -6,12 +6,7 @@ class Slugify
 {
     public function generate(string $input): string
     {
-        return trim(strtolower(preg_replace(['#[^A-Za-z0-9 -]+#', '#[\s-]+#'], ['', '-'], $this->removeSpecialCharacters($input))));
-    }
-
-    function removeSpecialCharacters($input)
-    {
-        $utf8 = [
+        $input = [
             '/[áàâãªä]/u' => 'a',
             '/[ÁÀÂÃÄ]/u' => 'A',
             '/[ÍÌÎÏ]/u' => 'I',
@@ -28,27 +23,6 @@ class Slugify
             '/Ñ/' => 'N',
             '/[«»]/u' => '',
         ];
-        return preg_replace(array_keys($utf8), array_values($utf8), $input);
+        return trim(strtolower(preg_replace(['#[^A-Za-z0-9 -]+#', '#[\s-]+#'], ['', '-'], $input)));
     }
-    //public function generate(string $input): string
-    //{
-     //   $input = [
-      //      '/[áàâãªä]/u' => 'a',
-      //      '/[ÁÀÂÃÄ]/u' => 'A',
-      //      '/[ÍÌÎÏ]/u' => 'I',
-      //      '/[íìîï]/u' => 'i',
-      //      '/[éèêë]/u' => 'e',
-     //       '/[ÉÈÊË]/u' => 'E',
-      //      '/[óòôõºö]/u' => 'o',
-      //      '/[ÓÒÔÕÖ]/u' => 'O',
-     //       '/[úùûü]/u' => 'u',
-      //      '/[ÚÙÛÜ]/u' => 'U',
-      //      '/ç/' => 'c',
-      //      '/Ç/' => 'C',
-      //      '/ñ/' => 'n',
-      //      '/Ñ/' => 'N',
-      //      '/[«»]/u' => '',
-       // ];
-       // return trim(strtolower(preg_replace(['#[^A-Za-z0-9 -]+#', '#[\s-]+#'], ['', '-'], $input)));
-    //}
 }
